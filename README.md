56b3cd5df2e3544131971a329de0d0071d726035
Shuhao Dong <shuhao.dong@bristol.ac.uk>
2025-06-18 14:17:22 +0100

    Integrate python wrapper

--- README.md
+++ README.md
@@ -9,13 +9,24 @@ This program is based on code by David G. Young at: https://github.com/davidgyou
 Please follow the instructions in the above link to install necessary libraries
 
 ## Test Platform
-Raspberry Pi 5 running Debian GNU/Linux version 12
+Raspberry Pi 5 running Debian GNU/Linux version 12, with Python 3.11.2
 
 ## Install Dependencies
 
-You have to install the following libraries before you can use this code
+You have to install the following libraries before you can use the source code scan_adv.c
 
     sudo apt-get install libssl-dev libbluetooth-dev
 
 Use with this firmware version to receive the extended advertisement packets: https://github.com/shuhao-dong/BORUS/tree/feature/ext-adv-sync
+
+Under the directory mqtt-publisher, there are two python files that: 
+
+(1) Test C wrapper subprocess
+(2) Test publisher code
+
+To run either python, you will need to install the mqtt library in your virtual environment 
+
+    pip install paho-mqtt
+
+
 
---
+++ mqtt-publisher/subprocess_test.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+"""
+check_scanner.py       verify that the C scanner subprocess
+                       produces well-formed JSON packets
+"""
+
+import json, subprocess, pprint, signal, sys
+
+SCANNER = "./scan_pub"          # path to your compiled C programme
+running = True
+
+def stop(sig, frame):
+    global running
+    running = False
+signal.signal(signal.SIGINT, stop)
+signal.signal(signal.SIGTERM, stop)
+
+# launch the scanner; text=True gives str lines instead of bytes
+proc = subprocess.Popen(["sudo", "-n", SCANNER, "1"], stdout=subprocess.PIPE,
+                        stderr=subprocess.STDOUT,
+                        text=True, bufsize=1)
+
+pp = pprint.PrettyPrinter(indent=2, width=80)
+
+try:
+    for line in proc.stdout:
+        if not running:
+            break
+        line = line.strip()
+        if not line:
+            continue                      # skip blank lines
+
+        try:
+            pkt = json.loads(line)        # validate & convert to dict
+        except json.JSONDecodeError as e:
+            print("JSON error:", e, "| raw:", line, file=sys.stderr)
+            continue
+
+        pp.pprint(pkt)                    # friendly dump
+except KeyboardInterrupt:
+    pass
+finally:
+    running = False
+    proc.terminate()
+    proc.wait()
