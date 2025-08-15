9accf0a1892f7318570fa7496cba1092f4970505
JoelDunnett <joeldunnett@gmail.com>
2025-08-15 15:53:59 +0100

    Program correctly outputs hPa instead of kPa

--- internship/arduino_pressure_program/arduino_pressure_program.ino
+++ internship/arduino_pressure_program/arduino_pressure_program.ino
@@ -1,43 +1,33 @@
-/*
-  Blink
 
-  Turns an LED on for one second, then off for one second, repeatedly.
-
-  Most Arduinos have an on-board LED you can control. On the UNO, MEGA and ZERO
-  it is attached to digital pin 13, on MKR1000 on pin 6. LED_BUILTIN is set to
-  the correct LED pin independent of which board is used.
-  If you want to know what pin the on-board LED is connected to on your Arduino
-  model, check the Technical Specs of your board at:
-  https://docs.arduino.cc/hardware/
-
-  modified 8 May 2014
-  by Scott Fitzgerald
-  modified 2 Sep 2016
-  by Arturo Guadalupi
-  modified 8 Sep 2016
-  by Colby Newman
-
-  This example code is in the public domain.
-
-  https://docs.arduino.cc/built-in-examples/basics/Blink/
-*/
-//Built-in library for barometer
+// Built-in library for barometer
 #include <Arduino_LPS22HB.h>
 
-int count;
-// the setup function runs once when you press reset or power the board
+// Note - do not include the humidity sensor as well!!
+// Including the library for the arduino HTS221 causes weird behaviour
+// This is likely due to the thermometer being shared between the humidity sensor and the pressure sensor
+
+// Used to filter the output to reduce noise
+float p_output = 0.0;
+const float alpha = 0.1;
+
 void setup() {
-  // initialize digital pin LED_BUILTIN as an output.
+  // Initialize digital pin LED_BUILTIN as an output.
   pinMode(LED_BUILTIN, OUTPUT);
+  // Initialize serial output for communication with NUC
   Serial.begin(9600);
+  // Initialzie barometer
   BARO.begin();
 }
 
-// the loop function runs over and over again forever
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
-  delay(500);                      // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by making the voltage LOW
-  delay(500);  
-  Serial.println(String(BARO.readPressure(),4));                // wait for a second
+  // Run loop once per second
+  int loop_start_time = millis();
+  if ((millis() - loop_start_time) < 1000) {
+    digitalWrite(LED_BUILTIN, HIGH);                              // Turn the LED on
+    p_output=(alpha*BARO.readPressure())+((1.0-alpha)*p_output);  // Filter pressure value to reduce noise
+    Serial.println(String(p_output,4));                           // Print up to 4 decimal points of pressure output to serial connection
+    delay(400);                                                   // Leave LED on for short time
+    digitalWrite(LED_BUILTIN, LOW);                               // Turn the LED off
+    delay(400);                                                   // Leave LED off for short time
+  }
 }
